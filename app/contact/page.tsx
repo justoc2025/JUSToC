9faab37e1f6f456e78482c71ab5ee5bb5e93cb63
justoc2025 <justoc2025@gmail.com>
2026-08-26 20:51:12 +0900

    Rebalance consultation headings

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -14,7 +14,6 @@ export default function ContactPage() {
             <a className="consult-wordmark" href="/" aria-label="JUSToC トップへ">
               <Image src="/justoc-logo-white-blue.png" alt="JUSToC" width={566} height={125} priority />
             </a>
-            <p className="consult-form-label">{content.intro.formLabel}</p>
             <h1 id="consult-title"><span>{content.intro.titleBefore}</span><span><em>{content.intro.titleAccent}</em>{content.intro.titleAfter}</span></h1>
             {content.intro.paragraphs.map((paragraph) => <p className="consult-lead" key={paragraph}>{paragraph}</p>)}
             <div className="consult-other-services" aria-labelledby="other-services-title">
